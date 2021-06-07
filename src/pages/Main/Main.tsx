45c148210db405d918c8d50da2964dfaff020f57
Maidar Erdyneev <fragma18@gmail.com>
2021-06-07 08:14:29 +0300

    fix arrows

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -30,7 +30,7 @@ const Main: React.FC = () => {
       <div className="page-copyright">
         <p className="page-copyright__school">
           <a href="https://bureau.ru/school" className="bureau-school-link">
-            Сделано в&nbsp;Школе&nbsp;Бюро&nbsp;Горбунова
+            <span className="bureau-school-link-text">Сделано в&nbsp;Школе&nbsp;Бюро&nbsp;Горбунова</span>
             <span className="bureau-school-link-icon" />
           </a>
         </p>
