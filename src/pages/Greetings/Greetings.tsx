2774013e6e3948ed0852986ad6f99d8ab2490624
Maidar Erdyneev <fragma18@gmail.com>
2021-06-16 13:13:41 +0300

    greetings page

--- src/pages/Greetings/Greetings.tsx
+++ src/pages/Greetings/Greetings.tsx
@@ -27,182 +27,177 @@ const Greetings: React.FC = () => {
             редактор&nbsp;Ксения&nbsp;Павлова,
             разработчики&nbsp;Майдар&nbsp;Эрдынеев и&nbsp;Александр&nbsp;Сушко
           </p>
-          <a
-            href="http://sergeychikin.ru/365/"
-            target="_blank"
-            styleName="authors__icons authors__icons_link"
-          >
-            Иконки Сергея&nbsp;Чикина
-          </a>
         </div>
-        <div styleName="content__inner columns">
-          <div styleName="column">
-            <div styleName="section">
-              <a href="https://type.today/" styleName="section__title">
-                Шрифты от&nbsp;TYPE TODAY
-              </a>
-              <div styleName="section__items">
-                <div styleName="section__item">
-                  <div styleName="section__item-title">Druk Wide</div>
-                  <div styleName="section__item-text">
-                    Бёртон&nbsp;Хасебе • Юрий&nbsp;Остроменцкий •
-                    Илья&nbsp;Рудерман • Commercial Type
-                  </div>
+        <div styleName="content__inner">
+          <div styleName="section">
+            <a
+              href="http://sergeychikin.ru/365/"
+              target="_blank"
+              styleName="section__title"
+            >
+              Иконки Сергея&nbsp;Чикина
+            </a>
+          </div>
+          <div styleName="section">
+            <a href="https://type.today/" styleName="section__title">
+              Шрифты от&nbsp;Тайп тудей
+            </a>
+            <div styleName="section__items">
+              <div styleName="section__item">
+                <div styleName="section__item-title">DRUK WIDE</div>
+                <div styleName="section__item-text">
+                  Бёртон&nbsp;Хасебе, Юрий&nbsp;Остроменцкий,
+                  Илья&nbsp;Рудерман, Commercial Type
                 </div>
-                <div styleName="section__item">
-                  <div styleName="section__item-title">STAG</div>
-                  <div styleName="section__item-text">
-                    Илья&nbsp;Рудерман • Commercial Type • Кристиан&nbsp;Шварц
-                  </div>
+              </div>
+              <div styleName="section__item">
+                <div styleName="section__item-title">STAG</div>
+                <div styleName="section__item-text">
+                  Илья&nbsp;Рудерман, Commercial Type, Кристиан&nbsp;Шварц
                 </div>
-                <div styleName="section__item">
-                  <div styleName="section__item-title">PILAR</div>
-                  <div styleName="section__item-text">
-                    Илья&nbsp;Рудерман • Юрий&nbsp;Остроменцкий •
-                    CSTM&nbsp;Fonts
-                  </div>
+              </div>
+              <div styleName="section__item">
+                <div styleName="section__item-title">PILAR</div>
+                <div styleName="section__item-text">
+                  Илья&nbsp;Рудерман, Юрий&nbsp;Остроменцкий, CSTM&nbsp;Fonts
                 </div>
-                <div styleName="section__item">
-                  <div styleName="section__item-title">AUSTIN</div>
-                  <div styleName="section__item-text">
-                    Пол&nbsp;Барнс • Commercial&nbsp;Type • Илья&nbsp;Рудерман
-                  </div>
+              </div>
+              <div styleName="section__item">
+                <div styleName="section__item-title">AUSTIN</div>
+                <div styleName="section__item-text">
+                  Пол&nbsp;Барнс, Commercial&nbsp;Type, Илья&nbsp;Рудерман
                 </div>
-                <div styleName="section__item">
-                  <div styleName="section__item-title">NORBERT SCHMAL</div>
-                  <div styleName="section__item-text">
-                    Филипп&nbsp;Ноймайер, TypeMates
-                  </div>
+              </div>
+              <div styleName="section__item">
+                <div styleName="section__item-title">NORBERT SCHMAL</div>
+                <div styleName="section__item-text">
+                  Филипп&nbsp;Ноймайер, TypeMates
                 </div>
-                <div styleName="section__item">
-                  <div styleName="section__item-title">GIORGIO SANS</div>
-                  <div styleName="section__item-text">
-                    Илья&nbsp;Рудерман • Винсент&nbsp;Чан • Кристиан&nbsp;Шварц
-                    • Commercial&nbsp;Type
-                  </div>
+              </div>
+              <div styleName="section__item">
+                <div styleName="section__item-title">GIORGIO SANS</div>
+                <div styleName="section__item-text">
+                  Илья&nbsp;Рудерман, Винсент&nbsp;Чан, Кристиан&nbsp;Шварц,
+                  Commercial&nbsp;Type
                 </div>
-                <div styleName="section__item">
-                  <div styleName="section__item-title">
-                    PARMIGIANO&nbsp;CAPTION&nbsp;PRO
-                  </div>
-                  <div styleName="section__item-text">
-                    Илья&nbsp;Рудерман • Рикардо&nbsp;Олоку •
-                    Джонатан&nbsp;Пьерини • Typotheque •
-                    Irina&nbsp;Smirnova&nbsp;(cyr) •
-                    Irene&nbsp;Vlachou&nbsp;(greek)
-                  </div>
+              </div>
+              <div styleName="section__item">
+                <div styleName="section__item-title">
+                  PARMIGIANO&nbsp;CAPTION&nbsp;PRO
                 </div>
-                <div styleName="section__item">
-                  <div styleName="section__item-title">CSTM Xprmntl 01</div>
-                  <div styleName="section__item-text">
-                    Илья&nbsp;Рудерман • CSTM&nbsp;Fonts •
-                    Юрий&nbsp;Остроменцкий
-                  </div>
+                <div styleName="section__item-text">
+                  Илья&nbsp;Рудерман, Рикардо&nbsp;Олоку,
+                  Джонатан&nbsp;Пьерини, Typotheque,
+                  Irina&nbsp;Smirnova&nbsp;(cyr),
+                  Irene&nbsp;Vlachou&nbsp;(greek)
                 </div>
-                <div styleName="section__item">
-                  <div styleName="section__item-title">NORMALIDAD EXTENDED</div>
-                  <div styleName="section__item-text">
-                    CSTM&nbsp;Fonts • Илья&nbsp;Рудерман •
-                    Юрий&nbsp;Остроменцкий • Анна&nbsp;Данилова
-                  </div>
+              </div>
+              <div styleName="section__item">
+                <div styleName="section__item-title">CSTM Xprmntl 01</div>
+                <div styleName="section__item-text">
+                  Илья&nbsp;Рудерман, CSTM&nbsp;Fonts, Юрий&nbsp;Остроменцкий
                 </div>
-                <div styleName="section__item">
-                  <div styleName="section__item-title">KARLOFF NEUTRAL</div>
-                  <div styleName="section__item-text">
-                    Никола&nbsp;Джурек • Петер&nbsp;Билак •
-                    Петер&nbsp;ван&nbsp;Росмален • Typotheque •
-                    Maria&nbsp;Doreuli&nbsp;(cyr&greek)
-                  </div>
+              </div>
+              <div styleName="section__item">
+                <div styleName="section__item-title">NORMALIDAD EXTENDED</div>
+                <div styleName="section__item-text">
+                  CSTM&nbsp;Fonts, Илья&nbsp;Рудерман, Юрий&nbsp;Остроменцкий, Анна&nbsp;Данилова
                 </div>
-                <div styleName="section__item">
-                  <div styleName="section__item-title">DISPLACE&nbsp;2.0</div>
-                  <div styleName="section__item-text">
-                    Денис&nbsp;Серебряков
-                  </div>
+              </div>
+              <div styleName="section__item">
+                <div styleName="section__item-title">KARLOFF NEUTRAL</div>
+                <div styleName="section__item-text">
+                  Никола&nbsp;Джурек, Петер&nbsp;Билак,
+                  Петер&nbsp;ван&nbsp;Росмален, Typotheque,
+                  Maria&nbsp;Doreuli&nbsp;(cyr&greek)
                 </div>
-                <div styleName="section__item">
-                  <div styleName="section__item-title">AMALTA</div>
-                  <div styleName="section__item-text">Вера&nbsp;Евстафьева</div>
-                </div>
+              </div>
+              <div styleName="section__item">
+                <div styleName="section__item-title">DISPLACE&nbsp;2.0</div>
+                <div styleName="section__item-text">Денис&nbsp;Серебряков</div>
+              </div>
+              <div styleName="section__item">
+                <div styleName="section__item-title">AMALTA</div>
+                <div styleName="section__item-text">Вера&nbsp;Евстафьева</div>
               </div>
             </div>
           </div>
-          <div styleName="column">
-            <div styleName="section">
-              <a href="https://www.paratype.ru/" styleName="section__title">
-                от&nbsp;PARATYPE
-              </a>
-              <div styleName="section__items">
-                <div styleName="section__item">
-                  <div styleName="section__item-title">Kudryashev Display</div>
-                  <div styleName="section__item-text">
-                    Ольга&nbsp;Умпелева • Изабелла&nbsp;Чаева •
-                    Александра&nbsp;Королькова
-                  </div>
-                </div>
-                <div styleName="section__item">
-                  <div styleName="section__item-title">VAST</div>
-                  <div styleName="section__item-text">
-                    Манвел&nbsp;Шмавонян • Александр&nbsp;Любовенко
-                  </div>
-                </div>
-                <div styleName="section__item">
-                  <div styleName="section__item-title">Circe Contrast</div>
-                  <div styleName="section__item-text">
-                    Александра&nbsp;Королькова • Изабелла&nbsp;Чаева
-                  </div>
-                </div>
-                <div styleName="section__item">
-                  <div styleName="section__item-title">Fence</div>
-                  <div styleName="section__item-text">
-                    Александра&nbsp;Королькова
-                  </div>
-                </div>
-                <div styleName="section__item">
-                  <div styleName="section__item-title">Fatum</div>
-                  <div styleName="section__item-text">
-                    Светлана&nbsp;Морозова
-                  </div>
-                </div>
-                <div styleName="section__item">
-                  <div styleName="section__item-title">Didona</div>
-                  <div styleName="section__item-text">Владимир&nbsp;Ефимов</div>
+          <div styleName="section">
+            <a href="https://www.paratype.ru/" styleName="section__title">
+              От&nbsp;Паратайпа
+            </a>
+            <div styleName="section__items">
+              <div styleName="section__item">
+                <div styleName="section__item-title">Kudryashev Display</div>
+                <div styleName="section__item-text">
+                  Ольга&nbsp;Умпелева, Изабелла&nbsp;Чаева,
+                  Александра&nbsp;Королькова
                 </div>
               </div>
+              <div styleName="section__item">
+                <div styleName="section__item-title">VAST</div>
+                <div styleName="section__item-text">
+                  Манвел&nbsp;Шмавонян, Александр&nbsp;Любовенко
+                </div>
+              </div>
+              <div styleName="section__item">
+                <div styleName="section__item-title">Circe Contrast</div>
+                <div styleName="section__item-text">
+                  Александра&nbsp;Королькова, Изабелла&nbsp;Чаева
+                </div>
+              </div>
+              <div styleName="section__item">
+                <div styleName="section__item-title">Fence</div>
+                <div styleName="section__item-text">
+                  Александра&nbsp;Королькова
+                </div>
+              </div>
+              <div styleName="section__item">
+                <div styleName="section__item-title">Fatum</div>
+                <div styleName="section__item-text">Светлана&nbsp;Морозова</div>
+              </div>
+              <div styleName="section__item">
+                <div styleName="section__item-title">Didona</div>
+                <div styleName="section__item-text">Владимир&nbsp;Ефимов</div>
+              </div>
             </div>
-            <div styleName="section">
-              <p styleName="section__title-without-link">Другие</p>
-              <div styleName="section__items">
-                <div styleName="section__item">
-                  <a
-                    href="https://thenorthernblock.co.uk/fonts/p/eldwin"
-                    styleName="section__item-title section__item-title_link"
-                  >
-                    Eldwin
-                  </a>
-                  <div styleName="section__item-text">
-                    The&nbsp;Northern&nbsp;Block
-                  </div>
+          </div>
+          <div styleName="section">
+            <p styleName="section__title-without-link">Другие шрифты</p>
+            <div styleName="section__items">
+              <div styleName="section__item">
+                <div styleName="section__item-title">Atyp</div>
+                <div styleName="section__item-text">
+                  Tomáš&nbsp;Brousil
                 </div>
-                <div styleName="section__item">
-                  <a
-                    href="https://www.behance.net/gallery/84911751/Misto-Font-Free-(Cyrillic-and-Latin)"
-                    styleName="section__item-title section__item-title_link"
-                  >
-                    Misto
-                  </a>
-                  <div styleName="section__item-text">
-                    Katerina&nbsp;Korolevtseva
-                  </div>
+              </div>
+              <div styleName="section__item">
+                <div styleName="section__item-title">TT Bluescreens</div>
+                <div styleName="section__item-text">
+                  Type Type
                 </div>
-                <div styleName="section__item">
-                  <div styleName="section__item-title">Ambidexter</div>
-                  <div styleName="section__item-text">Егор&nbsp;Белозёров</div>
+              </div>
+              <div styleName="section__item">
+                <div styleName="section__item-title">Eldwin</div>
+                <div styleName="section__item-text">
+                  The&nbsp;Northern&nbsp;Block
                 </div>
-                <div styleName="section__item">
-                  <div styleName="section__item-title">Harry plotter</div>
-                  <div styleName="section__item-text">Захар&nbsp;Ящин</div>
+              </div>
+              <div styleName="section__item">
+                <a styleName="section__item-title">
+                  Misto
+                </a>
+                <div styleName="section__item-text">
+                  Katerina&nbsp;Korolevtseva
                 </div>
+              </div>
+              <div styleName="section__item">
+                <div styleName="section__item-title">Harry plotter</div>
+                <div styleName="section__item-text">Захар&nbsp;Ящин</div>
+              </div>
+              <div styleName="section__item">
+                <div styleName="section__item-title">Ambidexter</div>
+                <div styleName="section__item-text">Егор&nbsp;Белозёров</div>
               </div>
             </div>
           </div>
